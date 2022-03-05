2b5c695d984a9e62f4b36a034ccd1b5d02e5643d
V <v.petrov@indev.expert>
2022-03-05 16:45:24 +0600

    Github Sync

--- library/src/main/java/kz/flyingv/aspectratiolayout/AspectRatioLayout.kt
+++ library/src/main/java/kz/flyingv/aspectratiolayout/AspectRatioLayout.kt
@@ -1,10 +1,8 @@
 package kz.flyingv.aspectratiolayout
 
-
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
-
 
 class AspectRatioLayout: FrameLayout{
 
